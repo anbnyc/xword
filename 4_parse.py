a4eb575635df146b4478ec75b4adffeabd232245
anbnyc <alecbarrett@gmail.com>
2017-02-14 18:03:27 -0500

    improve knowledge api search, add comments

--- 4_parse.py
+++ 4_parse.py
@@ -4,6 +4,7 @@ import functools
 import itertools
 # import spacy
 import numpy as np
+import re
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 
@@ -13,6 +14,11 @@ def cosine(v1, v2):
 	v2 = list(map(lambda x: float(x),v2))
 	return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
+"""
+Uses Google Knowledge Graph Search API to return articles
+	clue (str): clue to pass as API query
+	length (int): word length to filter response
+"""
 def getKnowledgeGraphCandidates(clue,length):
 	api_key = 'AIzaSyCz3EetlDMLlyU7LLWUH2n1U7mhUfqyxRk'
 	service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
@@ -23,12 +29,16 @@ def getKnowledgeGraphCandidates(clue,length):
 			'key': api_key,
 	}
 	url = service_url + '?' + urllib.parse.urlencode(params)
-	candidates = []
+	candidates = set()
 	response = json.loads(urllib.request.urlopen(url).read())
 	for element in response['itemListElement']:
 		try:
-			article = element["result"]["detailedDescription"]["articleBody"].split()
-			candidates += list(filter(lambda x: len(x) == length,article))
+			article = re.split(", |\. |\s",element["result"]["detailedDescription"]["articleBody"])
+			candidates.update(set(filter(lambda x: len(x) == length,article)))
+		except:
+			if element["result"].get("name"):
+				print("no results: ",element["result"]["name"])
+	return candidates
 
 """
 Uses NLTK WordNet
@@ -36,12 +46,13 @@ Uses NLTK WordNet
 	length (int): length of expected answer
 """
 def getWordnetCandidates(clue,length):
+	clue = re.sub('['+string.punctuation+']','',clue.lower())
 	if clue is not wn.morphy(clue):
 		## TODO reverse the morph transform
 		morphedclue = wn.morphy(clue)
 		morph = clue.replace(morphedclue,'')
 		clue = morphedclue
-		print(morph)
+		print("morph-",morph)
 	synsets = wn.synsets(clue)
 	names = functools.reduce(lambda x,y: x+y.lemma_names(),synsets,[])
 	for syn in synsets:
@@ -67,6 +78,10 @@ def getSpacyCandidates(clue,length,vocab,ret_count):
 	vocab.sort(key=lambda w: cosine(w.vector, vecsum))
 	return {w.orth_ for w in vocab[-1*ret_count:]}
 
+"""
+Takes NLTK vocabulary list and returns dict sorted by length
+	maxlen (int): longest word we want to store
+"""
 def sortVocab(maxlen):
 	sortedvocab = {}
 	keys = []
@@ -79,16 +94,17 @@ def sortVocab(maxlen):
 	return sortedvocab
 
 """
+Takes array of clues and appends candidate answers
 """
-def parseWords(clues):
+def getCandidates(clues):
 	vocab = sortVocab(15)
-	for i,v in enumerate(clues[2:]):
+	for i,v in enumerate(clues[52:]):
 		clue = v['clue']
 		length = v['length']
 		print(clue,length)
 		if clue.find(' ') == -1:
-			v["candidates_wordnet"] = getWordnetCandidates(clue.lower(),length)
-			print(v["candidates_wordnet"])
+			v["candidates_wordnet"] = getWordnetCandidates(clue,length)
+			print('candidates_wordnet',v["candidates_wordnet"])
 		elif "___" in clue:
 			print(clue)
 		else:
@@ -96,17 +112,38 @@ def parseWords(clues):
 			v["candidates_vec"] = getSpacyCandidates(clue_tokens,length,vocab[length],5)
 			clue_tokens = [nlp.vocab[x] for x in clue.split() if x not in stopwords.words('english')]
 			v["candidates_vec"].update(getSpacyCandidates(clue_tokens,length,vocab[length],5))
-			print(v["candidates_vec"])
-			clue_tokens = " ".join([x.orth_ for x in nlp(clue) if x.pos_ == "PROPN"])
-			v["candidates_know"] = getKnowledgeGraphCandidates(clue_tokens,length)
-			print(v["candidates_know"])
+			print('candidates_vec', v["candidates_vec"])
+			clue_tokens = getTokensForKGSearch(clue,True)
+			v["candidates_know"] = set()
+			if clue_tokens != '':
+				v["candidates_know"].update(getKnowledgeGraphCandidates(clue_tokens,length))
+			clue_tokens = getTokensForKGSearch(clue,False)
+			if clue_tokens != '':
+				v["candidates_know"].update(getKnowledgeGraphCandidates(clue_tokens,length))
+			print('candidates_know:', v["candidates_know"])
 		clues[i] = v
 	return clues
 
+"""
+Tokenize clue return words with specific POS
+	clue (str): clue to get pos_
+	properonly (bool): include only proper nouns
+"""
+def getTokensForKGSearch(clue,properonly = True):
+	clue = clue[:1].lower() + clue[1:]
+	tokens = nltk.pos_tag(nltk.word_tokenize(clue))
+	lookup = ["NNP","NNPS"] if properonly else ["NNP","NNPS","NN","NNS"]
+	ret = " ".join([i for (i,j) in tokens if j in lookup])
+	return ret
+
+"""
+Opens JSON file and passes clues to parse function
+	fileloc (str): path to JSON file
+"""
 def loadPuzzle(fileloc):
 	with open(fileloc,'r') as f:
 		puzzle = json.loads(f.read())[0]
-		puzzle['clues'] = parseWords(puzzle['clues'])
+		puzzle['clues'] = getCandidates(puzzle['clues'])
 		if input("print to file? y/n") == 'y':
 			f.write(json.dumps(puzzle,indent=1))
 
